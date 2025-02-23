81d25e2334f8b1ce268ff446fc41527b8afe5e8a
Augustin Briolon <matis.dene@edu.devinci.fr>
2025-02-23 18:35:36 +0100

    Inverser la transition du théâtre pour afficher le musée, ajouter une nouvelle section pour le musée avec des animations au défilement et intégrer une nouvelle police de caractères.

---
+++ src/components/SectionMuseum.tsx
@@ -0,0 +1,122 @@
+import { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const SectionMuseum = ({ children, className = '', id = '' }) => {
+  const sectionRef = useRef(null);
+  const contentRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      const textElements = contentRef.current.querySelectorAll('.anim-text');
+      const images = contentRef.current.querySelectorAll('.anim-img');
+      const longTextElements = contentRef.current.querySelectorAll('.anim-long-text');
+      const numberElements = contentRef.current.querySelectorAll('.anim-number');
+      
+      const scrollTriggerConfig = {
+        trigger: contentRef.current,
+        start: "top bottom-=100",
+        end: "bottom top",
+        toggleActions: "play none none reverse",
+        markers: true,
+      };
+
+      const mainTl = gsap.timeline({
+        scrollTrigger: scrollTriggerConfig,
+        defaults: {
+          ease: "power2.out",
+        }
+      });
+
+      if (textElements.length > 0) {
+        textElements.forEach((element: HTMLElement) => {
+          mainTl.fromTo(
+            element,
+            {
+              opacity: 0,
+              y: 100,
+            },
+            {
+              opacity: 1,
+              y: 0,
+              duration: 0.8,
+            },
+            "<+=0.1"
+          );
+        });
+      }
+
+      if (longTextElements.length > 0) {
+        mainTl.fromTo(
+          longTextElements,
+          {
+            opacity: 0,
+            y: 20,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 0.4,
+            stagger: 0.02,
+          },
+          "-=0.3"
+        );
+      }
+
+      if (numberElements.length > 0) {
+        mainTl.fromTo(
+          numberElements,
+          {
+            opacity: 0,
+            scale: 0,
+          },
+          {
+            opacity: 1,
+            scale: 1,
+            duration: 0.5,
+            ease: "back.out(2)",
+          },
+          "-=0.2"
+        );
+      }
+
+      if (images.length > 0) {
+        mainTl.fromTo(
+          images,
+          {
+            opacity: 0,
+            scale: 0.8,
+          },
+          {
+            opacity: 1,
+            scale: 1,
+            duration: 0.8,
+            ease: "back.out(1.7)",
+            stagger: 0.2,
+          },
+          "-=0.4"
+        );
+      }
+    }, sectionRef);
+
+    return () => {
+      ScrollTrigger.getAll().forEach(st => st.kill());
+      ctx.revert();
+    };
+  }, []);
+
+  return (
+    <div ref={sectionRef} id={id}>
+      <div
+        className={`h-dvh w-screen flex flex-col justify-center items-center gap-8 ${className}`}
+        ref={contentRef}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default SectionMuseum;
--- src/game/views/doms/reacts/MuseumReactView.tsx
+++ src/game/views/doms/reacts/MuseumReactView.tsx
@@ -2,18 +2,32 @@ import React, { useEffect } from 'react';
 import ReactViewBase, {
   TransitionProps,
 } from '../../../core/_engine/reacts/views/bases/ReactViewBase';
+import SectionMuseum from '../../../../components/SectionMuseum';
 
 const MuseumReactView: React.FC<TransitionProps> = (props) => {
+  const longTextSectionTwo =
+    'Le Musée trouve ses origines dans son rôle de protecteur, il fut ainsi construit pour protéger les orangers du palais des Tuileries durant les mois d’hiver.'.split(
+      ' '
+    );
+  const longTextSectionThree =
+    "L'esprit de la serre semble perdurer ici bas. La lumière et l'eau protègent l'arbre en son seins.".split(
+      ' '
+    );
+  const longTextSectionFive =
+    "Monet développe très tôt une fascination pour la lumière, les reflets et les variations infinies de la nature. Ce sont ces éléments qu’il cherchera à capturer dans son art, en privilégiant une peinture en plein air et des coups de pinceau vibrants. Son tableau 'Impression, soleil levant' inspirera alors le mouvement artistique de l’impressionisme".split(
+      ' '
+    );
+  const longTextSectionSix =
+    'Avec sa capacité à transformer la réalité en une vision vibrante et poétique, Claude Monet incarne non seulement l’essence de l’impressionnisme, mais aussi un lien profond entre la nature et l’art.'.split(
+      ' '
+    );
+
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       const scrollMax =
         document.documentElement.scrollHeight - window.innerHeight;
-
-      // Normalisation du scroll entre 0 et 1
       const scrollProgress = scrollY / scrollMax;
-
-      // Dispatch d'un événement personnalisé avec la valeur de scroll
       window.dispatchEvent(
         new CustomEvent('museumScroll', {
           detail: { progress: scrollProgress },
@@ -27,89 +41,31 @@ const MuseumReactView: React.FC<TransitionProps> = (props) => {
     };
   }, []);
 
-  
   return (
     <ReactViewBase {...props} className='w-screen h-fit z-50 relative'>
       <div className='borderScreenMuseum'></div>
-
       <div className='w-full overflow-y-scroll'>
-        <div className='h-dvh w-screen flex flex-col justify-center items-center gap-8'>
-          <h4 className='font-instrument-italic text-3xl'>Chapitre 01</h4>
-          <div className='flex flex-col justify-center items-center'>
-            <h2 className='font-nhaasgrotesk uppercase text-8xl'>
-              L'Orangerie
-            </h2>
-            <h3 className='font-instrument text-3xl uppercase'>
-              à l’origine il y a la serre
-            </h3>
-          </div>
-          <a href='#construction' className='bg-white px-1 py-4 rounded-full'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='black'
-              strokeWidth='1'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='animate-bounce'
-            >
-              <path d='M8 18L12 22L16 18' />
-              <path d='M12 2V22' />
-            </svg>
-          </a>
-        </div>
-
-        <div
-          id='construction'
-          className='h-dvh w-screen flex flex-col justify-center items-center gap-8'
-        >
-          <div className='flex flex-col justify-center items-center gap-2'>
-            <h2 className='font-nhaasgrotesk uppercase text-5xl'>1852</h2>
-            <h4 className='font-instrument text-2xl uppercase'>
-              (Construction)
+        <SectionMuseum>
+          <div className='overflow-hidden'>
+            <h4 className='anim-text font-instrument-italic text-3xl'>
+              Chapitre 01
             </h4>
           </div>
-
-          <h3 className='font-instrument text-3xl max-w-xl text-center'>
-            Le Musée trouve ses origines dans son rôle de protecteur, il fut
-            ainsi construit pour protéger les orangers du palais des Tuileries
-            durant les mois d’hiver.
-          </h3>
-        </div>
-
-        <div
-          id='dernier'
-          className='h-dvh w-[50vw] flex flex-col justify-center items-start gap-8 pl-10'
-        >
-          <div className='w-full flex justify-between items-center'>
-            <h2 className='font-instrument text-5xl'>Le dernier des Oranger</h2>
-            <p className='bg-black rounded-full w-5 h-5 p-4 flex items-center justify-center aspect-square font-instrument text-white text-xl'>
-              1
-            </p>
-          </div>
-
-          <h3 className='font-instrument text-2xl max-w-sm w-full'>
-            L'esprit de la serre semble perdurer ici bas. La lumière et l'eau
-            protègent l'arbre en son seins.
-          </h3>
-        </div>
-
-        <div className='h-dvh w-screen flex flex-col justify-center items-center gap-8'>
-          <h4 className='font-instrument-italic text-3xl'>Chapitre 02</h4>
           <div className='flex flex-col justify-center items-center'>
-            <h2 className='font-nhaasgrotesk uppercase text-8xl'>
-              Claude Monet
-            </h2>
-            <h3 className='font-instrument text-3xl uppercase'>
-              l'Architecte de la Lumière
-            </h3>
+            <div className='overflow-hidden'>
+              <h2 className='anim-text font-nhaasgrotesk-bold uppercase text-8xl'>
+                L'Orangerie
+              </h2>
+            </div>
+            <div className='overflow-hidden'>
+              <h3 className='anim-text font-instrument text-3xl uppercase'>
+                à l’origine il y a la serre
+              </h3>
+            </div>
           </div>
           <a
-            href='#impressionniste'
-            className='bg-white px-1 py-4 rounded-full'
+            href='#construction'
+            className='anim-text bg-white px-1 py-4 rounded-full'
           >
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -127,94 +83,79 @@ const MuseumReactView: React.FC<TransitionProps> = (props) => {
               <path d='M12 2V22' />
             </svg>
           </a>
-        </div>
+        </SectionMuseum>
 
-        <div
-          id='impressionniste'
-          className='h-dvh w-screen flex flex-col justify-center items-center gap-8'
-        >
-          <img
-            src='assets/images/autoportrait.webp'
-            alt='Peinture Autoportrait'
-            className='max-w-sm h-auto'
-          />
-          <h4 className='font-instrument text-5xl uppercase'>Claude Monet</h4>
-
-          <h3 className='font-nhaasgrotesk text-3xl max-w-xl text-center'>
-            Le père de l'impressionniste
+        <SectionMuseum id='construction'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <div className='overflow-hidden'>
+              <h2 className='anim-text font-nhaasgrotesk-bold uppercase text-5xl'>
+                1852
+              </h2>
+            </div>
+            <div className='overflow-hidden'>
+              <h4 className='anim-text font-instrument text-2xl uppercase'>
+                (Construction)
+              </h4>
+            </div>
+          </div>
+          <h3 className='overflow-hidde anim-long-text font-instrument text-3xl max-w-xl text-centern'>
+            {longTextSectionTwo.map((word, index) => (
+              <span key={index} className='inline-block overflow-hidden'>
+                <span className='anim-long-text inline-block'>
+                  {word}
+                  {index !== longTextSectionTwo.length - 1 && '\u00A0'}
+                </span>
+              </span>
+            ))}
           </h3>
-        </div>
+        </SectionMuseum>
 
-        <div className='h-dvh w-screen flex justify-center items-center gap-8'>
-          <div className='flex'>
-            <img
-              src='assets/images/soleil-levant.webp'
-              alt='Peinture Autoportrait'
-              className='max-w-sm h-auto'
-            />
-            <p className='bg-black rounded-full w-5 h-5 p-4 flex items-center justify-center aspect-square font-instrument text-white text-xl'>
-              2
+        <SectionMuseum className='!w-[50vw] pl-10'>
+          <div className='w-full flex justify-between items-center'>
+            <div className='overflow-hidden'>
+              <h2 className='anim-text font-instrument text-5xl'>
+                Le dernier des Orangers
+              </h2>
+            </div>
+            <p className='anim-number bg-black rounded-full w-5 h-5 p-4 flex items-center justify-center aspect-square font-instrument text-white text-xl'>
+              1
             </p>
           </div>
 
-          <div>
-            <h2 className='font-nhaasgrotesk uppercase text-5xl'>1852</h2>
-            <h4 className='font-instrument text-2xl uppercase'>
-              (Construction)
-            </h4>
-            <h3 className='ml-2 font-instrument text-3xl max-w-xl text-left'>
-              Monet développe très tôt une fascination pour la lumière, les
-              reflets et les variations infinies de la nature. Ce sont ces
-              éléments qu’il cherchera à capturer dans son art, en privilégiant
-              une peinture en plein air et des coups de pinceau vibrants. Son
-              tableau "Impression, soleil levant" inspirera alors le mouvement
-              artistique de l’impressionisme
-            </h3>
-          </div>
-        </div>
-
-        <div className='h-dvh w-screen flex flex-col justify-center items-center gap-8'>
-          <h3 className='font-instrument text-3xl max-w-xl text-center'>
-            Avec sa capacité à transformer la réalité en une vision vibrante et
-            poétique, Claude Monet incarne non seulement l’essence de
-            l’impressionnisme, mais aussi un lien profond entre la nature et
-            l’art.
+          <h3 className='font-instrument text-3xl max-w-xl text-centern'>
+            {longTextSectionThree.map((word, index) => (
+              <span key={index} className='inline-block overflow-hidden'>
+                <span className='anim-long-text inline-block'>
+                  {word}
+                  {index !== longTextSectionThree.length - 1 && '\u00A0'}
+                </span>
+              </span>
+            ))}
           </h3>
-          <div className='flex justify-center items-center gap-4'>
-            <img
-              src='assets/images/jardin.webp'
-              alt='Peinture Coclicot'
-              className='scale-75 w-52 h-72 object-cover'
-            />
-            <div className='relative'>
-              <img
-                src='assets/images/femme-ombrelle.webp'
-                alt='Peinture Coclicot'
-                className='w-52  h-72 object-cover'
-              />
-              <p className='abs-center bg-white rounded-full px-4 py-2 whitespace-nowrap text-center'>
-                Voir la galerie
-              </p>
-            </div>
-            <img
-              src='assets/images/coclicots.webp'
-              alt='Peinture Coclicot'
-              className='scale-75 w-52  h-72 object-cover'
-            />
-          </div>
-        </div>
+        </SectionMuseum>
 
-        <div className='h-dvh w-screen flex flex-col justify-center items-center gap-8'>
-          <h4 className='font-instrument-italic text-3xl'>Chapitre 03</h4>
-          <div className='flex flex-col justify-center items-center'>
-            <h2 className='font-nhaasgrotesk uppercase text-8xl'>
-              Les Nymphéas
-            </h2>
-            <h3 className='font-instrument text-3xl uppercase'>
-              l’ultime chef-dœuvre
-            </h3>
+        <SectionMuseum>
+          <div className='overflow-hidden'>
+            <h4 className='anim-text font-instrument-italic text-3xl'>
+              Chapitre 02
+            </h4>
           </div>
-          <a href='#' className='bg-white px-1 py-4 rounded-full'>
+          <div className='flex flex-col justify-center items-center'>
+            <div className='overflow-hidden'>
+              <h2 className='anim-text font-nhaasgrotesk-bold uppercase text-8xl'>
+                Claude Monet
+              </h2>
+            </div>
+            <div className='overflow-hidden'>
+              <h3 className='anim-text font-instrument text-3xl uppercase'>
+                l'Architecte de la Lumière
+              </h3>
+            </div>
+          </div>
+          <a
+            href='#impressionniste'
+            className='anim-text bg-white px-1 py-4 rounded-full'
+          >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='24'
@@ -231,7 +172,140 @@ const MuseumReactView: React.FC<TransitionProps> = (props) => {
               <path d='M12 2V22' />
             </svg>
           </a>
-        </div>
+        </SectionMuseum>
+
+        <SectionMuseum id='impressionniste'>
+          <div className='overflow-hidden'>
+            <img
+              src='assets/images/autoportrait.webp'
+              alt='Peinture Autoportrait'
+              className='anim-img max-w-sm h-auto'
+            />
+          </div>
+          <div className='overflow-hidden'>
+            <h4 className='font-instrument text-5xl uppercase'>Claude Monet</h4>
+          </div>
+
+          <div className='overflow-hidden'>
+            <h3 className='font-nhaasgrotesk text-3xl max-w-xl text-center'>
+              Le père de l'impressionniste
+            </h3>
+          </div>
+        </SectionMuseum>
+
+        <SectionMuseum className='!flex-row'>
+          <div className='flex'>
+            <div className='overflow-hidden'>
+              <img
+                src='assets/images/soleil-levant.webp'
+                alt='Peinture Autoportrait'
+                className='anim-img max-w-sm h-auto'
+              />
+            </div>
+            <p className='anim-number bg-black rounded-full w-5 h-5 p-4 flex items-center justify-center aspect-square font-instrument text-white text-xl'>
+              2
+            </p>
+          </div>
+
+          <div>
+            <div className='overflow-hidden'>
+              <h2 className='font-nhaasgrotesk-bold uppercase text-5xl'>
+                1852
+              </h2>
+            </div>
+            <div className='overflow-hidden'>
+              <h4 className='font-instrument text-2xl uppercase'>
+                (Construction)
+              </h4>
+            </div>
+            <h3 className='ml-2 font-instrument text-3xl max-w-xl text-left'>
+              {longTextSectionFive.map((word, index) => (
+                <span key={index} className='inline-block overflow-hidden'>
+                  <span className='anim-long-text inline-block'>
+                    {word}
+                    {index !== longTextSectionFive.length - 1 && '\u00A0'}
+                  </span>
+                </span>
+              ))}
+            </h3>
+          </div>
+        </SectionMuseum>
+
+        <SectionMuseum>
+          <h3 className='font-instrument text-3xl max-w-xl text-center'>
+            {longTextSectionSix.map((word, index) => (
+              <span key={index} className='inline-block overflow-hidden'>
+                <span className='anim-long-text inline-block'>
+                  {word}
+                  {index !== longTextSectionSix.length - 1 && '\u00A0'}
+                </span>
+              </span>
+            ))}
+          </h3>
+          <div className='flex justify-center items-center gap-4'>
+            <div className='overflow-hidden'>
+              <img
+                src='assets/images/jardin.webp'
+                alt='Peinture Coclicot'
+                className='anim-img scale-75 w-52 h-72 object-cover'
+              />
+            </div>
+            <div className='relative'>
+              <div className='overflow-hidden'>
+                <img
+                  src='assets/images/femme-ombrelle.webp'
+                  alt='Peinture Coclicot'
+                  className='anim-img w-52  h-72 object-cover'
+                />
+              </div>
+              <p className='anim-number abs-center bg-white rounded-full px-4 py-2 whitespace-nowrap text-center'>
+                Voir la galerie
+              </p>
+            </div>
+            <div className='overflow-hidden'>
+              <img
+                src='assets/images/coclicots.webp'
+                alt='Peinture Coclicot'
+                className='anim-img scale-75 w-52  h-72 object-cover'
+              />
+            </div>
+          </div>
+        </SectionMuseum>
+
+        <SectionMuseum>
+          <div className='overflow-hidden'>
+            <h4 className='font-instrument-italic text-3xl'>Chapitre 03</h4>
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <div className='overflow-hidden'>
+              <h2 className='font-nhaasgrotesk-bold uppercase text-8xl'>
+                Les Nymphéas
+              </h2>
+            </div>
+            <div className='overflow-hidden'>
+              <h3 className='font-instrument text-3xl uppercase'>
+                l’ultime chef-dœuvre
+              </h3>
+            </div>
+          </div>
+          <a href='#' className='anim-text bg-white px-1 py-4 rounded-full'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='black'
+              strokeWidth='1'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='animate-bounce'
+            >
+              <path d='M8 18L12 22L16 18' />
+              <path d='M12 2V22' />
+            </svg>
+          </a>
+        </SectionMuseum>
       </div>
     </ReactViewBase>
   );
