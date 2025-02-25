00262a699066f8c8f00adb975d892fca421e60ae
Matis DENE <matis.dene@edu.devinci.fr>
2025-02-25 14:39:27 +0100

    update museum scene

--- src/game/views/threes/LobbyThreeView.ts
+++ src/game/views/threes/LobbyThreeView.ts
@@ -1,16 +1,13 @@
-import { AdditiveBlending, AmbientLight, BackSide, CameraHelper, DoubleSide, Mesh, MeshBasicMaterial, MeshPhysicalMaterial, MeshStandardMaterial, Object3D, PlaneGeometry, PMREMGenerator, PointLight, SpotLight, SpotLightHelper, TextureLoader, Vector2 } from "three";
+import { ThreeAssetsManager } from "@cooker/three";
+import { DoubleSide, Mesh, MeshPhysicalMaterial, MeshStandardMaterial, Object3D, PlaneGeometry, PointLight, Vector2 } from "three";
+import { AssetId } from "../../constants/games/AssetId";
 import { Object3DId } from "../../constants/games/Object3DId";
 import { ViewId } from "../../constants/views/ViewId";
 import { ViewPlacementId } from "../../constants/views/ViewPlacementId";
 import { ThreeCameraControllerBase } from "../../core/_engine/threejs/cameras/bases/ThreeCameraControllerBase";
-import { MainThree } from "../../core/_engine/threejs/MainThree";
 import { Object3DsProxy } from "../../core/_engine/threejs/proxies/Object3DsProxy";
 import { ThreeCamerasProxy } from "../../core/_engine/threejs/proxies/ThreeCamerasProxy";
 import { WithoutTransitionThreeView } from "../../core/_engine/threejs/views/WithoutTransitionThreeView";
-import { LobbyThreeTheater } from "../../theaters/LobbyThreeTheater";
-import { TheatersProxy } from "pancake";
-import { AssetId } from "../../constants/games/AssetId";
-import { ThreeAssetsManager } from "@cooker/three";
 
 export default class LobbyThreeView extends WithoutTransitionThreeView {
     private _lobbyMesh: Object3D;
@@ -56,15 +53,9 @@ export default class LobbyThreeView extends WithoutTransitionThreeView {
             emissiveIntensity: 0.9,
         });
         this._secondClouds = this._clouds.clone();
-        // this.add(this._clouds);
-        // this.add(this._titlePlane);
         window.addEventListener('updateCameraPosition', this._onUpdateCameraPosition.bind(this));
         this._pointLight = new PointLight(0xffffff, 250, 100);
         this._pointLight.position.set(0, 10, 0);
-        // this.add(this._pointLight);
-        // const spotLight = new SpotLight(0xffffff, 80, 500, Math.PI, 0.25, 1);
-        // spotLight.position.set(2, 0, 0);
-        // this.add(spotLight);
 
         const glassMaterial = new MeshPhysicalMaterial({
             metalness: .7,
@@ -72,12 +63,9 @@ export default class LobbyThreeView extends WithoutTransitionThreeView {
             envMapIntensity: 0.9,
             clearcoat: 1,
             transparent: true,
-            // transmission: .95,
             opacity: .8,
             reflectivity: 0.2,
-            // refractionRatio: 0.985,
             ior: 0.9,
-            // side: BackSide,
         });
         const whiteMaterial = new MeshPhysicalMaterial({
             color: 0xffffff,
@@ -114,8 +102,6 @@ export default class LobbyThreeView extends WithoutTransitionThreeView {
             }
             if (child.name === Object3DId.LOBBY_MIDDLE_BLOC) {
                 const basMat = new MeshStandardMaterial({ map: ThreeAssetsManager.GetTexture(AssetId.TEXTURE_MIDDLE_BLOC) });
-                // const basMat = new MeshBasicMaterial({ color: 0xff0000 });
-
                 child.traverse((child: Object3D) => {
                     if (child instanceof Mesh) {
                         child.material = basMat;
@@ -125,8 +111,6 @@ export default class LobbyThreeView extends WithoutTransitionThreeView {
 
             if (child.name === Object3DId.LOBBY_FLOAR) {
                 const basMat = new MeshStandardMaterial({ map: ThreeAssetsManager.GetTexture(AssetId.TEXTURE_FLOAR) });
-                // const basMat = new MeshBasicMaterial({ color: 0xff0000 });
-
                 child.traverse((child: Object3D) => {
                     if (child instanceof Mesh) {
                         child.material = basMat;
@@ -136,8 +120,6 @@ export default class LobbyThreeView extends WithoutTransitionThreeView {
 
             if (child.name === Object3DId.LOBBY_WALL) {
                 const basMat = new MeshStandardMaterial({ map: ThreeAssetsManager.GetTexture(AssetId.TEXTURE_WALL_BAKE) });
-                // const basMat = new MeshBasicMaterial({ color: 0xff0000 });
-
                 child.traverse((child: Object3D) => {
                     if (child instanceof Mesh) {
                         child.material = basMat;
@@ -155,7 +137,6 @@ export default class LobbyThreeView extends WithoutTransitionThreeView {
 
     private _initMouseListener(): void {
         window.addEventListener("mousemove", (event) => {
-            // Normaliser la position du curseur dans l'espace 3D
             this._mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
             this._mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
         });
@@ -172,10 +153,8 @@ export default class LobbyThreeView extends WithoutTransitionThreeView {
         const oscillationSpeed = 0.003; // Vitesse de l'oscillation
         const oscillationAmplitude = 12; // Amplitude de l'oscillation
 
-        // Calcul fluide de la position en Y
         this._clouds.position.x = Math.sin(this._time * 0.1 * oscillationSpeed) * oscillationAmplitude;
         this._secondClouds.position.x = -Math.sin(this._time * 0.1 * oscillationSpeed) * oscillationAmplitude;
-        // Calcul des rotations basées sur la position de la souris
         const rotationX = this._mouse.y * this._cameraRotationFactor;
         const rotationY = this._mouse.x * this._cameraRotationFactor;
         this._camera.rotation.x += (rotationX - this._camera.rotation.x) * 0.1;
